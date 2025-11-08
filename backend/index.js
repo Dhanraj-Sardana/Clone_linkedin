33be9410802624886dc939c45241e47f62e84ad7
Dhanraj-Sardana <dhanrajsardana2003@gmail.com>
2025-11-08 22:23:16 +0530

    render commit

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDb();
 
 const app = express();
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  origin:'https://clone-linkedin-backend.onrender.com' || 'http://localhost:5173',
   credentials: true,
 }));
 
