0721c24648e482b3875116100006b16d4c03419b
Dhanraj-Sardana <dhanrajsardana2003@gmail.com>
2025-11-08 23:08:49 +0530

    google redirect

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ connectDb();
 
 const app = express();
 app.use(cors({
-  origin:'https://clone-linkedin-backend.onrender.com' || 'http://localhost:5173',
+  origin:'https://clone-linkedin-alpha.vercel.app' || 'http://localhost:5173',
   credentials: true,
 }));
 
